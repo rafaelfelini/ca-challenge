115044c57c3afa504374ff174f7baf1394b6168f
Willy Camargo <camargowilly@gmail.com>
2017-06-04 17:08:09 -0300

    change Button tests descriptions

--- src/__tests__/components/Button.js
+++ src/__tests__/components/Button.js
@@ -6,7 +6,7 @@ it('renders without crashing', () => {
   shallow(<Button label="Foo bar" />);
 });
 
-it('with click event - render a `button` element', () => {
+it('onClick property - render a `button` element', () => {
   const div = document.createElement('div');
   const onClick = jest.fn();
   const button = shallow(<Button label="Foo bar" onClick={onClick}/>, div);
@@ -16,10 +16,9 @@ it('with click event - render a `button` element', () => {
   expect(onClick).toBeCalled();
 });
 
-it('with url property - render a `Link` component (react-router)', () => {
+it('url property - render a `Link` component (react-router)', () => {
   const div = document.createElement('div');
   const button = shallow(<Button label="Foo bar" url='foo/bar'/>, div);
-
 
   expect(button.prop('to')).toEqual('foo/bar');
 });
