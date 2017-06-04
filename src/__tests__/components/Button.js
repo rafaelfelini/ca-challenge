847c1966b840886be6885db39f7161993f7524c7
Willy Camargo <camargowilly@gmail.com>
2017-06-04 17:07:17 -0300

    add Button tests

--- src/__tests__/components/Button.js
+++ src/__tests__/components/Button.js
@@ -3,6 +3,23 @@ import { shallow } from 'enzyme';
 import Button from '../../components/Button';
 
 it('renders without crashing', () => {
+  shallow(<Button label="Foo bar" />);
+});
+
+it('with click event - render a `button` element', () => {
   const div = document.createElement('div');
-  shallow(<Button />, div);
+  const onClick = jest.fn();
+  const button = shallow(<Button label="Foo bar" onClick={onClick}/>, div);
+
+  button.simulate('click');
+
+  expect(onClick).toBeCalled();
+});
+
+it('with url property - render a `Link` component (react-router)', () => {
+  const div = document.createElement('div');
+  const button = shallow(<Button label="Foo bar" url='foo/bar'/>, div);
+
+
+  expect(button.prop('to')).toEqual('foo/bar');
 });
