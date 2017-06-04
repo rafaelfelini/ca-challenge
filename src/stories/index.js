30e955003583deeb6e4a865f1599eae35a596411
Willy Camargo <camargowilly@gmail.com>
2017-06-04 15:17:58 -0300

    add Header on stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
+import Header from '../components/Header';
+import '../index.css';
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+storiesOf('Header', module)
+  .add('default', () => (
+    <Header />
   ));
