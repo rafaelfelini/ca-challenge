e19f2b19942e9e79a6a2b4bb51558884d55c2e40
Willy Camargo <camargowilly@gmail.com>
2017-06-05 00:43:38 -0300

    fix lint errors

--- src/components/VehicleListContainer.js
+++ src/components/VehicleListContainer.js
@@ -2,31 +2,6 @@ import React, { Component } from 'react';
 import VehicleList from './VehicleList';
 import firebaseDatabase from '../utils/firebase/database';
 
-const mockedData = [
-  {
-    combustivel: 'Flex',
-    marca: 'Volkswagem',
-    modelo: 'Gol',
-    placa: 'FFF-5498',
-    valor: '20000'
-  },
-  {
-    combustivel: 'Gasolina',
-    marca: 'Volkswagem',
-    modelo: 'Fox',
-    placa: 'FOX-4125',
-    valor: '20000'
-  },
-  {
-    combustivel: 'Alcool',
-    imagem: 'http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg',
-    marca: 'Volkswagen',
-    modelo: 'Fusca',
-    placa: 'PAI-4121',
-    valor: '20000'
-  }
-]
-
 class VehicleListContainer extends Component {
   constructor(props) {
     super(props);
